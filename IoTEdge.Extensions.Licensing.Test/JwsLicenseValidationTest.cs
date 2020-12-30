cf139d8128690c06bfa2cff9d926584c20ebd90f
Lars Kemmann <lars.kemmann@bynalogic.com>
2020-12-30 17:01:15 -0500

    Unit tests & bugfixes

--- IoTEdge.Extensions.Licensing.Test/JwsLicenseValidationTest.cs
+++ IoTEdge.Extensions.Licensing.Test/JwsLicenseValidationTest.cs
@@ -10,40 +10,185 @@ namespace IoTEdge.Extensions.Licensing.Test
     [TestClass]
     public class JwsLicenseValidationTest
     {
-        [TestMethod]
-        public void ValidLicenseStatusIsReturnedCorrectly()
+        static readonly X509SecurityKey issuerCertificateWithPrivateKey;
+        static readonly X509SecurityKey issuerCertificatePublicKeyOnly;
+        static readonly JwtSecurityTokenHandler jwtHandler = new JwtSecurityTokenHandler();
+
+        // For simplicity, all tests should be defined relative to this timestamp.
+        static readonly DateTime now = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+
+        static JwsLicenseValidationTest()
         {
-            var jwtHandler = new JwtSecurityTokenHandler();
+            // The self-signed certificates in this project are valid from 2020-12-30 21:37:41 UTC until 2040-12-30 21:37:41 UTC.
+            issuerCertificateWithPrivateKey = new X509SecurityKey(X509Certificate2.CreateFromPemFile(@".\issuer.cer", @".\issuer.key"));
+            issuerCertificatePublicKeyOnly = new X509SecurityKey(new X509Certificate2(@".\issuer.cer"));
+        }
 
-            var issuerPrivateKey = new X509SecurityKey(new X509Certificate2(Base64UrlEncoder.DecodeBytes(
-                @"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAMcbxmGS/pQ6ulE1xlgPydiJxLfMBLhpXyT7aQv/zuZLNFnjqo+dtogIRckEdHdnPrLdKwVqIcg/3HCwSIeQrOFWpHpgrSTAjy0y0atKLA2LP79bJsjSTGh/BGHAWajYQsaRYNggnPW5O8p4rmPNGUXEeR6Lm0xlgHoTqng2vEDRAgMBAAECgYBk6GCsBtFa0kCm87fn3WiQg5HdDFrAEzcTCQ9981EqSabof4drbaSaYIbtj4JvGTYfdBNflSA12pefzNeVTO8wB9h+oG7wj3YCcoQSra34k79PY53uGks3G5aqcPPpMjC6ojIrNmHeSFKECKwSRqdmKKvgFzn6CXDR6TLTaNohmQJBAOVNASQaUg6D+icuvGTzSEdz4yU4KKmzWoHelthPICst1wgH3RKLX9cVgBonNiraon77Zk1ksSayyF4rhGwZO5MCQQDeSsx0v/N3D5P421QBaT/VSSVlE7d6468l/+bfXBQ+kiWbGYVbExtBDgJDZYESEaZmctC2vn85mhrDaP8T9HiLAkAB+vZFj4yh33XrnLW30XoQU+nkSmXfgVMIyBlZaOWIOe8ffKHmJRoAy4i9sRUArb61hgpOJM563RRp68pK/LTNAkEA2vOB/10ySgumDHC8hcdNgJ/TnYOWLg0l75/noAnqRtddAzBYEiT3q1RJFmlcgJex9ycQPW/VkL8hrWg2F0mtkQJAD8KXA/qXgdn40mKGuMWtqbdGQvqQ9j9ODfwlqXqV7GzkBt4XFm8fcZtluKeyzGzuLVD3ZmMdaApsu+sk6iJ5Jg==")));
 
-            var issuerPublicKey = new X509SecurityKey(new X509Certificate2(Base64UrlEncoder.DecodeBytes(
-                @"MIICVDCCAb2gAwIBAgIBADANBgkqhkiG9w0BAQ0FADBHMQswCQYDVQQGEwJ1czELMAkGA1UECAwCTkMxEjAQBgNVBAoMCUFjbWUgSW5jLjEXMBUGA1UEAwwObGljZW5zaW5nLnRlc3QwHhcNMjAxMjI5MjM0MzE0WhcNMjExMjI5MjM0MzE0WjBHMQswCQYDVQQGEwJ1czELMAkGA1UECAwCTkMxEjAQBgNVBAoMCUFjbWUgSW5jLjEXMBUGA1UEAwwObGljZW5zaW5nLnRlc3QwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMcbxmGS/pQ6ulE1xlgPydiJxLfMBLhpXyT7aQv/zuZLNFnjqo+dtogIRckEdHdnPrLdKwVqIcg/3HCwSIeQrOFWpHpgrSTAjy0y0atKLA2LP79bJsjSTGh/BGHAWajYQsaRYNggnPW5O8p4rmPNGUXEeR6Lm0xlgHoTqng2vEDRAgMBAAGjUDBOMB0GA1UdDgQWBBQQ10LXCor/5F6OcrbHXjd2LvRQIDAfBgNVHSMEGDAWgBQQ10LXCor/5F6OcrbHXjd2LvRQIDAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBDQUAA4GBAHyp95Wh2iHXEQXQQ1FcnxqKt9/VmU1t+zKFJnZWTUuD9XxDvWsM5fPHMTdVYu6qpYi46aTO1iWdhW2C84hhU/eJynkmXvRwxBaeIT1d9fr1sG8U1SYVKxLnywCDnRkijezLOtZRsS1nj+NDJXTQu4zJnpuDflTQufVgflWpKrfw")));
+        [TestMethod]
+        public void ValidLicenseIsValidatedWithoutErrors()
+        {
+            var token = CreateJws(id: "12345", audience: "testaudience", issuer: "testissuer",
+                expires: now.AddDays(1), issuedAt: now, notBefore: now.AddMinutes(-10),
+                module: "modulename", device: "myedgehost", iothub: "myiothub",
+                issuerSigningKey: issuerCertificateWithPrivateKey);
 
-            var now = new DateTime(2020, 12, 30, 0, 0, 0, DateTimeKind.Utc);
+            JwsLicenseValidation.Validate(token, issuerCertificatePublicKeyOnly,
+                expectedTokenId: "12345", validAudience: "testaudience", validIssuer: "testissuer",
+                now, moduleInstanceName: "modulename", hostName: "myedgehost", hubName: "myiothub");
+        }
+
+        [TestMethod]
+        public void ExpiredLicenseErrorIsThrownCorrectly()
+        {
+            var token = CreateJws(id: "12345", audience: "testaudience", issuer: "testissuer",
+                expires: now.AddDays(-1), issuedAt: now.AddDays(-2), notBefore: now.AddDays(-2),
+                module: "modulename", device: "myedgehost", iothub: "myiothub",
+                issuerSigningKey: issuerCertificateWithPrivateKey);
+
+            Assert.ThrowsException<SecurityTokenInvalidLifetimeException>(() =>
+                JwsLicenseValidation.Validate(token, issuerCertificatePublicKeyOnly,
+                    expectedTokenId: "12345", validAudience: "testaudience", validIssuer: "testissuer",
+                    now, moduleInstanceName: "modulename", hostName: "myedgehost", hubName: "myiothub"));
+        }
+
+        [TestMethod]
+        public void NotYetValidLicenseErrorIsThrownCorrectly()
+        {
+            var token = CreateJws(id: "12345", audience: "testaudience", issuer: "testissuer",
+                expires: now.AddDays(2), issuedAt: now.AddDays(-2), notBefore: now.AddDays(1),
+                module: "modulename", device: "myedgehost", iothub: "myiothub",
+                issuerSigningKey: issuerCertificateWithPrivateKey);
+
+            Assert.ThrowsException<SecurityTokenInvalidLifetimeException>(() =>
+                JwsLicenseValidation.Validate(token, issuerCertificatePublicKeyOnly,
+                    expectedTokenId: "12345", validAudience: "testaudience", validIssuer: "testissuer",
+                    now, moduleInstanceName: "modulename", hostName: "myedgehost", hubName: "myiothub"));
+        }
+
+        [TestMethod]
+        public void IncorrectAudienceErrorIsThrownCorrectly()
+        {
+            var token = CreateJws(id: "12345", audience: "testaudience", issuer: "testissuer",
+                expires: now.AddDays(1), issuedAt: now, notBefore: now.AddMinutes(-10),
+                module: "modulename", device: "myedgehost", iothub: "myiothub",
+                issuerSigningKey: issuerCertificateWithPrivateKey);
+
+            Assert.ThrowsException<SecurityTokenInvalidAudienceException>(() =>
+                JwsLicenseValidation.Validate(token, issuerCertificatePublicKeyOnly,
+                    expectedTokenId: "12345", validAudience: "INCORRECT", validIssuer: "testissuer",
+                    now, moduleInstanceName: "modulename", hostName: "myedgehost", hubName: "myiothub"));
+        }
+
+        [TestMethod]
+        public void IncorrectIssuerErrorIsThrownCorrectly()
+        {
+            var token = CreateJws(id: "12345", audience: "testaudience", issuer: "testissuer",
+                expires: now.AddDays(1), issuedAt: now, notBefore: now.AddMinutes(-10),
+                module: "modulename", device: "myedgehost", iothub: "myiothub",
+                issuerSigningKey: issuerCertificateWithPrivateKey);
+
+            Assert.ThrowsException<SecurityTokenInvalidIssuerException>(() =>
+                JwsLicenseValidation.Validate(token, issuerCertificatePublicKeyOnly,
+                    expectedTokenId: "12345", validAudience: "testaudience", validIssuer: "INCORRECT",
+                    now, moduleInstanceName: "modulename", hostName: "myedgehost", hubName: "myiothub"));
+        }
+
+        [TestMethod]
+        public void IncorrectIoTHubNameErrorIsThrownCorrectly()
+        {
+            var token = CreateJws(id: "12345", audience: "testaudience", issuer: "testissuer",
+                expires: now.AddDays(1), issuedAt: now, notBefore: now.AddMinutes(-10),
+                module: "modulename", device: "myedgehost", iothub: "myiothub",
+                issuerSigningKey: issuerCertificateWithPrivateKey);
+
+            Assert.ThrowsException<Exception>(() =>
+                JwsLicenseValidation.Validate(token, issuerCertificatePublicKeyOnly,
+                    expectedTokenId: "12345", validAudience: "testaudience", validIssuer: "testissuer",
+                    now, moduleInstanceName: "modulename", hostName: "myedgehost", hubName: "INCORRECT"));
+        }
+
+        [TestMethod]
+        public void IncorrectHostNameErrorIsThrownCorrectly()
+        {
+            var token = CreateJws(id: "12345", audience: "testaudience", issuer: "testissuer",
+                expires: now.AddDays(1), issuedAt: now, notBefore: now.AddMinutes(-10),
+                module: "modulename", device: "myedgehost", iothub: "myiothub",
+                issuerSigningKey: issuerCertificateWithPrivateKey);
+
+            Assert.ThrowsException<Exception>(() =>
+                JwsLicenseValidation.Validate(token, issuerCertificatePublicKeyOnly,
+                    expectedTokenId: "12345", validAudience: "testaudience", validIssuer: "testissuer",
+                    now, moduleInstanceName: "modulename", hostName: "INCORRECT", hubName: "myiothub"));
+        }
+
+        [TestMethod]
+        public void IncorrectModuleNameErrorIsThrownCorrectly()
+        {
+            var token = CreateJws(id: "12345", audience: "testaudience", issuer: "testissuer",
+                expires: now.AddDays(1), issuedAt: now, notBefore: now.AddMinutes(-10),
+                module: "modulename", device: "myedgehost", iothub: "myiothub",
+                issuerSigningKey: issuerCertificateWithPrivateKey);
+
+            Assert.ThrowsException<Exception>(() =>
+                JwsLicenseValidation.Validate(token, issuerCertificatePublicKeyOnly,
+                    expectedTokenId: "12345", validAudience: "testaudience", validIssuer: "testissuer",
+                    now, moduleInstanceName: "INCORRECT", hostName: "myedgehost", hubName: "myiothub"));
+        }
+
+        [TestMethod]
+        public void UnspecifiedModuleNameIsValidatedWithoutErrors()
+        {
+            var token = CreateJws(id: "12345", audience: "testaudience", issuer: "testissuer",
+                expires: now.AddDays(1), issuedAt: now, notBefore: now.AddMinutes(-10),
+                module: null, device: "myedgehost", iothub: "myiothub",
+                issuerSigningKey: issuerCertificateWithPrivateKey);
+
+            JwsLicenseValidation.Validate(token, issuerCertificatePublicKeyOnly,
+                expectedTokenId: "12345", validAudience: "testaudience", validIssuer: "testissuer",
+                now, moduleInstanceName: "modulename", hostName: "myedgehost", hubName: "myiothub");
+        }
+
+        [TestMethod]
+        public void IncorrectTokenIdErrorIsThrownCorrectly()
+        {
+            var token = CreateJws(id: "12345", audience: "testaudience", issuer: "testissuer",
+                expires: now.AddDays(1), issuedAt: now, notBefore: now.AddMinutes(-10),
+                module: "modulename", device: "myedgehost", iothub: "myiothub",
+                issuerSigningKey: issuerCertificateWithPrivateKey);
+
+            Assert.ThrowsException<Exception>(() =>
+                JwsLicenseValidation.Validate(token, issuerCertificatePublicKeyOnly,
+                    expectedTokenId: "INCORRECT", validAudience: "testaudience", validIssuer: "testissuer",
+                    now, moduleInstanceName: "modulename", hostName: "myedgehost", hubName: "myiothub"));
+        }
+
+
+        private static string CreateJws(string id, string audience, string issuer,
+            DateTime expires, DateTime issuedAt, DateTime notBefore,
+            string module, string device, string iothub,
+            X509SecurityKey issuerSigningKey)
+        {
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Audience = "AUDIENCE",
-                Expires = now.AddDays(1),
-                IssuedAt = now,
-                Issuer = "ISSUER",
-                NotBefore = now.AddMinutes(-10),
-                SigningCredentials = new SigningCredentials(issuerPrivateKey, SecurityAlgorithms.HmacSha256)
+                Audience = audience,
+                Expires = expires,
+                IssuedAt = issuedAt,
+                Issuer = issuer,
+                NotBefore = notBefore,
+                SigningCredentials = new SigningCredentials(issuerSigningKey, SecurityAlgorithms.RsaSha256)
             };
+
             tokenDescriptor.Claims = new Dictionary<string, object>
             {
-                ["jti"] = "12345",
-                ["module"] = "modulename",
-                ["device"] = "myedgehost",
-                ["iothub"] = "myiothub"
+                ["jti"] = id,
+                ["module"] = module,
+                ["device"] = device,
+                ["iothub"] = iothub
             };
 
-            var token = jwtHandler.CreateEncodedJwt(tokenDescriptor);
-
-            JwsLicenseValidation.Validate(token, issuerPublicKey,
-                expectedTokenId: "12345", validAudience: "AUDIENCE", validIssuer: "ISSUER",
-                now, moduleInstanceName: "modulename", hostName: "myedgehost", hubName: "myiothub");
+            return jwtHandler.CreateEncodedJwt(tokenDescriptor);
         }
     }
 }
--- IoTEdge.Extensions.Licensing/JwsLicenseValidation.cs
+++ IoTEdge.Extensions.Licensing/JwsLicenseValidation.cs
@@ -45,15 +45,15 @@ namespace IoTEdge.Extensions.Licensing
             if (expectedTokenId != null && validatedToken.Id != expectedTokenId)
                 throw new Exception("The token ID in the license did not match the expected value");
 
-            if (claimsPrincipal.FindFirst("device").Value != moduleInstanceName)
+            if (claimsPrincipal.FindFirst("device").Value != hostName)
                 throw new Exception("The IoT Edge device name in the license did not match the expected value");
 
-            if (claimsPrincipal.FindFirst("iothub").Value != moduleInstanceName)
+            if (claimsPrincipal.FindFirst("iothub").Value != hubName)
                 throw new Exception("The IoT Hub name in the license did not match the expected value");
 
             // The module constraint is optional -- enforce it only if it was specified in the JWS.
             var moduleClaim = claimsPrincipal.FindFirst("module")?.Value;
-            if (moduleClaim != null && moduleClaim != moduleInstanceName)
+            if (!string.IsNullOrEmpty(moduleClaim) && moduleClaim != moduleInstanceName)
                 throw new Exception("The module instance name was specified in the license and did not match the expected value");
         }
     }
